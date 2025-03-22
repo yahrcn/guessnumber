2106127b3b9c767256c6736ac9d023b9eed18850
Nikita Parygin <n.parygin@skbkontur.ru>
2025-03-23 02:01:23 +0400

    bugfix

--- src/App.js
+++ src/App.js
@@ -57,7 +57,6 @@ export default function App() {
             const j = Math.floor(Math.random() * (i + 1));
             [arr[i], arr[j]] = [arr[j], arr[i]];
         }
-        console.log(desiredNumber);
 
         return arr.join('');
     };
